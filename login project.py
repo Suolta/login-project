bcea2c8b9eed76869d10c0b09b57514fdc0577b5
Sultan <76474756+Suolta@users.noreply.github.com>
2021-01-28 13:26:19 -0500

    Add files via upload

---
+++ login project.py
@@ -0,0 +1,15 @@
+username = 'admin'      #user
+password = 'admin1'    #pass
+
+one = str(input('Please Enter a username:  '))     #to input the username
+two = str(input('Please Enter a password:  '))     #to input the password
+
+if one == username and two == password:        #to the pas = user 
+  print('hello admin')      #to print
+else:               
+  print('Sorry..!')
+  print('the username is \"admin\" and the password is \"admin1\"')
+  print(input('>>>>>: '))
+  print(input('>>>>>: '))
+   print('Done....!')
+
